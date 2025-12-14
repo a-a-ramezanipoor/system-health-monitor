f9ff49f2f30102dcdee06c6c704c5a8b196f4984
amir <amir@amir.fardaad.co>
2025-12-14 15:12:06 +0330

    feat: add CLI argument parser with placeholders

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-echo "hello world"
+process_args(){
 
+if [ "$#" -eq 0 ]; then
+	echo "show help"
+	return 0
+else
+	for ARG in "$@"; do
+		case "$ARG" in
+			"--help")
+				echo "help";;
+			"--cpu")
+				echo "cpu";;
+			"--memory")
+				echo "memory";;
+			"--disk")
+				echo "disk";;
+			*)
+				echo "Invalid argument: $ARG"
+				return 1
+		esac
+	done
+fi
+
+}
+
+process_args "$@"
